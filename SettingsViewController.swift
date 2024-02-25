37fba90d5ae88cd47997278f473b546107a423e2
Wylan Neely <wylanneely@gmail.com>
2024-02-25 15:49:13 -0800

    SettingsViewController
    -added tap to disable numbers pad
    -added calendarView with appropriate dates
    -added calendarviewdelegates and date picker delegates

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UICalendarViewDelegate, UICalendarSelectionSingleDateDelegate  {
     
     
     let soberController = SoberController()
@@ -15,6 +15,9 @@ class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+           view.addGestureRecognizer(tap)
         
         if soberData == nil {
             let todaysDate = Date()
@@ -24,13 +27,42 @@ class SettingsViewController: UIViewController {
             
             dateTextField.text = dformatter.string(from: todaysDate)
         }
+        
+        createCalendar()
 
     }
     
-
-    // MARK: - Outlets
+    func createCalendar() {
+        
+        let calendarView = UICalendarView()
+        calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        calendarView.calendar = .current
+        calendarView.locale = .current
+        calendarView.fontDesign = .rounded
+        calendarView.delegate = self
+        let dateSelection = UICalendarSelectionSingleDate(delegate: self)
+        calendarView.selectionBehavior = dateSelection
+        calendarContainerView.addSubview(calendarView)
+        
+        let startDate = Date(timeIntervalSince1970: 0)
+        calendarView.availableDateRange = DateInterval(start: startDate, end: Date())
+        NSLayoutConstraint.activate([calendarView.leadingAnchor.constraint(equalTo: calendarContainerView.leadingAnchor, constant: 0),
+                                     calendarView.trailingAnchor.constraint(equalTo: calendarContainerView.trailingAnchor, constant: 0),
+                                     calendarView.topAnchor.constraint(equalTo: calendarContainerView.topAnchor, constant: 0),
+                                     calendarView.bottomAnchor.constraint(equalTo: calendarContainerView.bottomAnchor, constant: 0)
+                                    ])
+    }
     
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    //MARK: - Outlets
+    
+    @IBOutlet weak var calendarContainerView: UIView!
     @IBOutlet weak var dateTextField: UITextField!
+    
+   
     
     /*
     // MARK: - Navigation
@@ -42,4 +74,35 @@ class SettingsViewController: UIViewController {
     }
     */
 
+
+
+    
+    
+    
+    //MARK: - Delegate
+
+// Control whether a person can select a given date.
+    func dateSelection(_ selection: UICalendarSelectionSingleDate,
+    canSelectDate dateComponents: DateComponents?) -> Bool {
+    // Allow all dates by returning true if the selection parameter contains
+    // a date component instance. Prevent someone from clearing the selection
+    // by returning false if the selection parameter is nil.
+        return dateComponents != nil
+    }
+
+
+    func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
+        return nil
+    }
+    
+    func calendarView(_ calendarView: UICalendarView, didChangeVisibleDateComponentsFrom previousDateComponents: DateComponents) {
+        
+    }
+    
+    func dateSelection(_ selection: UICalendarSelectionSingleDate, didSelectDate dateComponents: DateComponents?) {
+        
+    }
+    
+    
+    
 }
