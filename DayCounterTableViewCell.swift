40e9fa706ec81656b428a672763efbf3ff68808c
Wylan Neely <wylanneely@gmail.com>
2024-02-27 22:10:20 -0800

    Created DayCounterTableviewCell and Xib

---
+++ DayCounterTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  DayCounterTableViewCell.swift
+//  MySoberDate
+//
+//  Created by Wylan L Neely on 2/27/24.
+//
+
+import UIKit
+
+class DayCounterTableViewCell: UITableViewCell {
+    
+    var soberData: SoberData?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setUpLabels(){
+        if let soberData = soberData {
+            timeLabel.text = "\(soberData.returnDaysSober())"
+            timeTypeLabel.text = "Days"
+        }
+        
+    }
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timeTypeLabel: UILabel!
+    
+    
+}
