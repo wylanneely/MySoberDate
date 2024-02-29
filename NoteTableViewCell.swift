3595ca055d3955427acb407e685e6e8a3692d323
Wylan Neely <wylanneely@gmail.com>
2024-02-28 18:00:59 -0800

    updated TableViewCells
    -changed to clear color
    -resized to be matching

---
+++ NoteTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoteTableViewCell.swift
+//  MySoberDate
+//
+//  Created by Wylan L Neely on 2/27/24.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
