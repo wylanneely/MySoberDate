1ef73c684f5e252be05aeff23b9aac796662b3ee
Wylan Neely <wylanneely@gmail.com>
2024-03-01 21:37:43 -0800

    updated Date class
    - added variable that printed date as a string to be comparable with other dates without using time

--- Extensions.swift
+++ Extensions.swift
@@ -16,4 +16,39 @@ extension Calendar {
         let numberOfDays = dateComponents([.day], from: from, to: to)
         return numberOfDays.day!
     }
+    
+}
+
+extension Date {
+
+    var onlyDate: Date? {
+        get {
+            let calender = Calendar.current
+            var dateComponents = calender.dateComponents([.year, .month, .day], from: self)
+            dateComponents.timeZone = .current
+            return calender.date(from: dateComponents)
+        }
+    }
+    
+    func from(year: Int, month: Int, day: Int) -> Date {
+            let gregorianCalendar = NSCalendar(calendarIdentifier: .gregorian)!
+
+            var dateComponents = DateComponents()
+            dateComponents.year = year
+            dateComponents.month = month
+            dateComponents.day = day
+
+            let date = gregorianCalendar.date(from: dateComponents)!
+            return date
+        }
+    
+    var asDateString: String? {
+        get {
+            let dformatter = DateFormatter()
+            dformatter.dateStyle = .medium
+            dformatter.timeStyle = .none
+            
+            return dformatter.string(from: self)
+        }
+    }
 }
