1c13a394e4f7ecff1aecda6a36b94182592316bc
Wylan Neely <wylanneely@gmail.com>
2024-03-03 16:09:31 -0800

    updated Quotes object
    -20 new quotes and image references

--- Quotes.swift
+++ Quotes.swift
@@ -11,7 +11,7 @@ import UIKit
 
 struct Quotes {
     
-    let quotes: [Quote] = [quote1]
+    let quotes: [Quote] = [quote5]
     
     
     
@@ -20,3 +20,23 @@ struct Quotes {
 
 
 let quote1 = Quote(quote: "Sometimes you have to be a lion, so that you can be the lamb you really are.", author: "Dave Chappelle",image: UIImage(named: "dave-chappelle")!)
+let quote2 = Quote(quote: "I do not like that man, I must get to know him better.", author: "Abraham Lincoln", image: UIImage(named: "abraham-lincoln")!)
+let quote3 = Quote(quote: "The best and most beautiful things in the world cannot be seen or even touched... they must be felt with the heart", author: "Helen Keller", image: UIImage(named: "helen-keller")!)
+let quote4 = Quote(quote: "Darkness cannot drive out darkness, only light can do that. Hate cannot drive out hate, only love can do that.", author: "Martin L. King Jr.", image: UIImage(named: "mlk")!)
+let quote5 = Quote(quote: "Once you know what failure feels like, determination chases success.", author: "Kobe Bryant", image: UIImage(named: "kobe-bryant")!)
+let quote6 = Quote(quote: "So you were born to feel ‘nice?’ Instead of doing things and experiencing them? Why aren’t you running to do what your nature demands?", author: "Marcus Aurelius", image: UIImage(named: "marcus-aurelius")!)
+let quote7 = Quote(quote: "Do not pray for easy lives. Pray to be stronger men.", author: "Rev. Phillips Brooks", image: UIImage(named: "rev-phillips-brooks")!)
+let quote8 = Quote(quote: "If freedom of speech is taken away, then dumb and silent we may be led, like sheep to the slaughter.", author: "George Washington", image: UIImage(named: "george-washington")!)
+let quote9 = Quote(quote: "A new idea must not be judged by its immediate results", author: "Nikola Tesla", image: UIImage(named: "nikola-tesla")!)
+let quote11 = Quote(quote:"If you want to be the best, you have to do things that other people aren't willing to do.", author: "Michael Phelps", image: UIImage(named: "michael-phelps")!)
+let quote10 = Quote(quote: "No, I don't ever give up. I'd have to be dead or completely incapacitated", author: "Elon Musk", image: UIImage(named: "elon-musk")!)
+let quote12 = Quote(quote: "The cynics may be the loudest voices, but I promise you, they will accomplish the least", author: "Barack Obama", image: UIImage(named: "obama")!)
+let quote13 = Quote(quote: "Without passion, you don't have energy; without energy, you have nothing. Nothing great in the world has been accomplished without passion!", author: "Donald J. Trump", image: UIImage(named: "trump")!)
+let quote14 = Quote(quote: "If you ever start taking things too seriously, just remember that we are talking monkeys on an organic spaceship flying through the universe.", author: "Joe Rogan", image: UIImage(named: "joe-rogan")!)
+let quote15 = Quote(quote: "If I have seen further than others, it is by standing on the shoulders of Giants.", author: "Isaac Newton", image: UIImage(named: "isaac-newton")!)
+let quote16 = Quote(quote: "The most difficult thing is the decision to act, the rest is merely tenacity", author: "Amelia Earhart", image: UIImage(named: "earhart")!)
+let quote17 = Quote(quote: "You must never be fearful about what you are doing when it is right", author: "Rosa Parks", image: UIImage(named: "rosa-parks")!)
+let quote18 = Quote(quote: "Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world.", author: "Harriet Tubman", image: UIImage(named: "harriet-tubman")!)
+let quote19 = Quote(quote: "The most courageous act is still to think for yourself aloud.", author: "Coco Chanel", image: UIImage(named: "coco")!)
+let quote20 = Quote(quote: "Humans are allergic to change. They love to say, 'We've always done it this way.' I try to fight that.", author: "Grace Hopper", image: UIImage(named: "grace-hopper")!)
+
