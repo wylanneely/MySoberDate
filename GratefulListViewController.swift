277e6ffc6aeff816ab209be97b0edc57a6454d12
Wylan Neely <wylanneely@gmail.com>
2024-03-18 20:30:38 -0700

    updated GratefulListController
    -less code

--- GratefulListViewController.swift
+++ GratefulListViewController.swift
@@ -44,6 +44,9 @@ class GratefulListViewController: UIViewController, UITableViewDelegate, UITable
         }
     }
     
+    
+    //MARK: - TableView
+    
     @IBOutlet weak var tableView: UITableView!
     
     func setUpTableView(){
@@ -72,15 +75,5 @@ class GratefulListViewController: UIViewController, UITableViewDelegate, UITable
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 420
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
